50ca8ff33593dcfbf9ea51ee4e2f69159175ac28
Emil Haugberg van Veen <emilveen@gmail.com>
2019-01-11 16:54:55 +0000

    Remove obsolete files.

--- src/commands.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.help = "List of possible commands are\n\
-- nlp\n\
-- blah blah";
-exports.invalid = "Invalid command. Type 'help' for a list of commands.";
-exports.hello = "Invalid command. Type 'help' for a list of commands.";
